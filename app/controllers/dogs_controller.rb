c755ef11e17790ae7c3255154377032eaf0407db
Arthur Lurye <arthurlurye@gmail.com>
2018-04-17 09:49:26 -0400

    built pet shop

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+
+    def index
+      @dogs = Dog.all
+    end
+
+    def show
+      @dog = Dog.find(params[:id])
+    end
+
+    def new
+      @dog = Dog.new
+    end
+
+    def create
+
+    end
+
+    def update
+      @dog = Dog.update(params[:id])
+    end
+
+    def edit
+      @dog = Dog.find(params[:id])
+    end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+
+    @employee = Employee.create(emp_params)
+        if @employee.valid?
+        redirect_to @employee
+      else
+        flash[:errors] = @employee.errors.full_messages
+        redirect_to new_employee_path
+      end
+  end
+
+  def update
+      @employee = Employee.find(params[:id])
+      @employee.update(emp_params)
+      if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to edit_employee_path
+    end
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+    @dogs = Dog.all
+  end
+
+  private
+
+  def emp_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office,:img_url,
+    :dog_id)
+  end
 end
